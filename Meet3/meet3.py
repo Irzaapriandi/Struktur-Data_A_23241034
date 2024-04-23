f2cdc844e1968849395713bad4ee6f3a404009e8
Irzaapriandi <145946462+Irzaapriandi@users.noreply.github.com>
2024-04-23 12:14:25 +0800

    Add files via upload
    meet3 Python

---
+++ Meet3/meet3.py
@@ -0,0 +1,26 @@
+# belajar tipe data
+
+# tipe data integer
+bil_integer = 100
+print (type(bil_integer))
+print ("tipe data dari bil_integer adalah : ", (type(bil_integer)))
+
+# tipe data float : bilangan desimal
+bil_float = 2.5
+print ("tipe data dari bil_float adalah : ",(type(bil_float)))
+print (type(bil_float))
+
+# tipe data boolean : 0/1, true/false, ya/tidak
+bil_bol = False
+print (type(bil_bol))
+print ("tipe data dari bil_bool adalah : ", (type(bil_bol)))
+
+# tipe data string : kumpulan karakter
+bil_stirng = "300000.5"
+print (type(bil_stirng))
+print ("tipe data dari bil_string adalah : ", (type(bil_stirng)))
+
+# tipe data dari c
+from ctypes import c_double
+bil_double(300000000)
+print (type(bil_double))
