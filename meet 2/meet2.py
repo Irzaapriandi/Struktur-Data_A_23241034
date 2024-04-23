381cf90eed97c3b1ac3ed519e02362c8ed10c9fe
Irzaapriandi <145946462+Irzaapriandi@users.noreply.github.com>
2024-04-23 12:21:49 +0800

    Add files via upload

---
+++ meet 2/meet2.py
@@ -0,0 +1,14 @@
+# hari ini kita belajar Variabel
+# Assigment value
+
+x = 3 # assigment pertama
+print ("Nilai UTS sy adalah" , x)
+x = 6
+y = 9
+print ("Nilai UAS sy adalah" , y)
+
+jumlah_a = 100  # tidak boleh spasi
+bungkus10 = 5000    # tidak boleh diawali angka
+
+x = 1000 # assigment kedua
+print ("Nilai UAS2 sy adalah" , x)
