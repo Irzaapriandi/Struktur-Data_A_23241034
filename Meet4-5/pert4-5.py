a4a06f236398335e2c02808b9f53667514dd02c9
Irzaapriandi <145946462+Irzaapriandi@users.noreply.github.com>
2024-05-07 23:02:20 +0800

    Add files via upload

---
+++ Meet4-5/pert4-5.py
@@ -0,0 +1,50 @@
+# Inputan
+# Aritmatika + - x :
+
+# coba = input ("Masukkan Nama : ")
+# print(coba)
+
+# Bilangan_random = input("Masukkan bilanga acak :")
+# print(bilangan_random)
+
+# Aritmatika
+x = 9
+y = 3
+
+# Penjumlahan +
+hasil = x + y
+print(x, '+' ,y ,'=' , hasil)
+
+# Pengurangan -
+hasil = x - y
+print(x, '-' ,y ,'=' , hasil)
+
+# Perkalian x
+hasil = x * y
+print(x, 'x' ,y ,'=' , hasil)
+
+# Perpangkatan **
+hasil = x ** y
+print(x, '' ,y ,'=' , hasil)
+
+# Modulus %
+hasil = x % y
+print(x, 'mod' ,y ,'=' , hasil)
+
+# Ploor division //
+hasil = x // y
+print(x, 'floor' ,y ,'=' , hasil)
+
+
+# Skala prioritas (), **, */:, +/-
+x = 3
+y = 4
+z = 5
+hasil = (x ** y * (z + x) / y - z % x // y)
+print(hasil)
+print( x, '**', y, '*', z, '+', x, '/', y, '-', z, '%', x, '//', y, '=', hasil)
+
+
+
+
+
