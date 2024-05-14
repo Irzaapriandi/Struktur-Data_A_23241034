8ba15e2301db1b80bfb1d2d548d1b4df4c54674b
Irzaapriandi <145946462+Irzaapriandi@users.noreply.github.com>
2024-05-14 17:06:28 +0800

    Add files via upload

---
+++ meet6/meet6.py
@@ -0,0 +1,40 @@
+# Perbandingan >, <, >=, <=, ==, !=, is, is not
+
+x = 3
+y = 4
+
+# Lebih besar >
+hasil = y > x
+print (x, '>', y, '=', hasil)
+
+# Lebih besar >
+hasil = y < x
+print (x, '<', y, '=', hasil)
+
+# Lebih kecil sama dengan <=
+hasil = y <= x
+print (x, '<=', y, '=', hasil)
+print (x, '<=', 3, '=', hasil)
+
+# Lebih besar sama dengan >=
+hasil = y >= x
+print (x, '>=', y, '=', hasil)
+print (x, '>=', 3, '=', hasil)
+
+# == (sama dengan)
+hasil = x == 9
+print (x, '==', 9, '=', hasil)
+
+# != (tidak sama dengan)
+hasil = x != 9
+print (x, '!=', 9, '=', hasil)
+
+# is (adalah)
+hasil = x is 9
+print(x, 'is', 9, '=', hasil)
+
+# is not (bukan)
+hasil = x is not 9
+print(x, 'is not', 9, '=', hasil)
+
+
