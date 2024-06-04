905089afdd3c541446a8a61cc1a38ee9f40e02d3
Irzaapriandi <145946462+Irzaapriandi@users.noreply.github.com>
2024-06-04 20:37:13 +0800

    Add files via upload

---
+++ meet8/meet8.py
@@ -0,0 +1,22 @@
+# Belajar percabangan
+
+nama = input("Masukkan Nama : ")
+
+# if kondisi : aksi (standar)
+#if nama=="edy": print("Dosen Ganteng")
+#print("Program Selesai")
+
+# if kondisi : aksi (indentasi)
+#if nama=="edy":
+ #   print("dosen ganteng")
+ #   print("ganteng baget")
+#print("Program Selesai")
+
+if nama=="edy":
+    print("dosen ganteng")
+    print(f"ganteng baget, makasih {nama}")
+else:
+    print(f"ah gak ganteng, tanks {nama}")
+print("Program Selesai")
+
+
