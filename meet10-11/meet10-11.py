7de21b70e03fec81c7cd8e65e99259aa4b7b9751
Irzaapriandi <145946462+Irzaapriandi@users.noreply.github.com>
2024-06-25 11:57:41 +0800

    Add files via upload
    meet 10-11

---
+++ meet10-11/meet10-11.py
@@ -0,0 +1,36 @@
+# belajar perulangan (Loop)
+
+#bil = 1
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+
+#for kondisi :
+#    aksi 
+
+#penggunaan loop pada pemrograman lain
+#for (int i; i<=10; i++) {
+#    aksi
+#    aksi
+#}
+
+#perulangan menggunakan list
+bil = [0, 1, 11, 3, 9]
+print(bil)
+
+for i in bil :
+    print(f"nilai i sekarang => {i}")
+print("akhir program 1") 
+
+bil2 = range(3)
+for i in bil2 :
+    #print(f"nilai i sekarang => {i}")   
+    print("pak edy dosen ganteng") 
+
+#for bil in range(10) :
+#    aksi
+#    aksi
